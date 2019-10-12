c37f627018c1540442d7d7cff0141e237cfdc9e6
yvesshum <yvesshum1210@gmail.com>
2019-10-12 00:55:40 -0500

    progress on notifications

--- paypalproject/App.js
+++ paypalproject/App.js
@@ -29,19 +29,91 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Registration from './src/registration.js';
+import NotificationPopup from 'react-native-push-notification-popup';
+
+var PushNotification = require("react-native-push-notification");
+
+PushNotification.configure({
+
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function(token) {
+        console.log( 'TOKEN:', token );
+    },
+
+    // (required) Called when a remote or local notification is opened or received
+    onNotification: function(notification) {
+        console.log( 'NOTIFICATION:', notification );
+    },
+
+    // IOS ONLY (optional): default: all - Permissions to register.
+    permissions: {
+        alert: true,
+        badge: true,
+        sound: true
+    },
+
+    // Should the initial notification be popped automatically
+    // default: true
+    popInitialNotification: true,
+
+    /**
+     * (optional) default: true
+     * - Specified if permissions (ios) and token (android and ios) will requested or not,
+     * - if not, you must call PushNotificationsHandler.requestPermissions() later
+     */
+    requestPermissions: true,
+});
+
+
 
 class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
+
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Home Screen</Text>
                 <Text>Hi</Text>
                 <Button onPress={() => this.props.navigation.navigate('Registration')} title="Go To Registration"/>
+                <Button onPress={() => {
+                    // PushNotification.localNotification({
+                    //     /* iOS only properties */
+                    //
+                    //     /* iOS and Android properties */
+                    //     title: "My Notification Title", // (optional)
+                    //         message: "My Notification Message", // (required)
+                    //     playSound: false, // (optional) default: true
+                    //     soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+                    //
+                    //     repeatType: 'day', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
+                    //     actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
+                    // });
 
+
+                    PushNotification.localNotificationSchedule({
+                        //... You can use all the options from localNotifications
+                        message: "My Notification Message", // (required)
+                        date: new Date(Date.now() + 10 * 1000) // in 60 secs
+                    });
+                    this.popup.show({
+                        onPress: function() {console.log('Pressed')},
+                        appTitle: 'Glim',
+                        timeText: 'Now',
+                        title: 'Hey',
+                        body: 'This is a sample message.',
+                        slideOutTime: 5000
+                    });
+                }} title="GET NOTIFICATIONS"/>
+                <NotificationPopup ref={ref => this.popup = ref} />
 
             </View>
         );
     }
+
 }
 
 const AppNavigator = createStackNavigator({
