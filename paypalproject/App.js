3c232c839b361651244060bc5b65d88d31cd218e
yvesshum <yvesshum1210@gmail.com>
2019-10-11 21:40:48 -0500

    test change

--- paypalproject/App.js
+++ paypalproject/App.js
@@ -39,6 +39,7 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
+              <Text>Hello from Yves</Text>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
